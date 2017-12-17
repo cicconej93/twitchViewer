9467081bfb4cdad1806d2ddf9aeb29f784628d4d
spoodermang <cicconej93@gmail.com>
2017-12-17 14:10:07 -0500

    use work around twitch API call

--- project/script.js
+++ project/script.js
@@ -1,13 +1,12 @@
-var selection = "all";
-var globalJsonVar;
-var on = "";
-var off = "";
 
-$.getJSON('https://gist.githubusercontent.com/QuincyLarson/2ff6892f948d0b7118a99264fd9c1ce8/raw/e9e12f154d71cf77fc32e94e990749a7383ca2d6/Twitch%2520sample%2520API%2520responses%2520in%2520array%2520form', function(data){
-	globalJsonVar = data;
-	selectAll();
-});
+var globalJsonVar = [];
+var selection = "";
 
+var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+
+getData();
+setTimeout(function() {selectAll(); }, 1000);
 
 
 $(".col-but").on("click", function(){
@@ -28,52 +27,38 @@ $(".col-but").on("click", function(){
 
 
 function selectAll(){
-	var len = globalJsonVar.length;
-	for(var i = 0; i < globalJsonVar.length-1; i++){
-		$(".streams").append("<div class='stream'></div>")
-	} 
-	var j = 0;
-	$(".stream").each(function(){
-		if(this.innerText == ""){		
-			if(globalJsonVar[j].stream !== null){
-				$(this).html('<a href='+globalJsonVar[j].stream.url+'><span>'+globalJsonVar[j].stream.display_name + '<img src='+globalJsonVar[j].stream.logo+'></span><p>'+globalJsonVar[j].stream.status+'</p></a>');
-			} else {
-				$(this).html('<span>'+globalJsonVar[j].display_name+'</span>');
-			}		
-		} j++;
-		if($(this).text == ""){
-			$(this).fadeOut();
+	for(var i = 0; i < globalJsonVar.length; i++){
+		if(globalJsonVar[i].stream == null){
+			$(".streams").append("<div class='stream'><span>"+streamers[i]+"</span></div>");
+		} else {
+			$(".streams").append("<div class='stream'><a href="+globalJsonVar[i].stream.channel.url+"><span>"+globalJsonVar[i].stream.channel.display_name+" <img src="+globalJsonVar[i].stream.channel.logo+"></span><p>"+globalJsonVar[i].stream.channel.game+"</p></a></div>");
 		}
-	});
+	}
 }
 
 function selectOn(){
-	var len = globalJsonVar.length;
-	for(var i = 0; i < globalJsonVar.length-1; i++){
-		$(".streams").append("<div class='stream'></div>")
-	} 
-	var j = 0;
-	$(".stream").each(function(){
-		if(this.innerText == ""){		
-			if(globalJsonVar[j].stream !== null){
-				$(this).html('<a href='+globalJsonVar[j].stream.url+'><span>'+globalJsonVar[j].stream.display_name + '<img src='+globalJsonVar[j].stream.logo+'></span><p>'+globalJsonVar[j].stream.status+'</p></a>');
-			} 		
-		} j++;
-		if(this.innerText == ""){
-			$(this).remove();
-		}
-	});
-
-
+	for(var i = 0; i < globalJsonVar.length; i++){
+		if(globalJsonVar[i].stream !== null){
+			$(".streams").append("<div class='stream'><a href="+globalJsonVar[i].stream.channel.url+"><span>"+globalJsonVar[i].stream.channel.display_name+" <img src="+globalJsonVar[i].stream.channel.logo+"></span><p>"+globalJsonVar[i].stream.channel.game+"</p></a></div>");
+		} 
+	}
 }
 
 function selectOff(){
-	for(var i =0; i < globalJsonVar.length-1; i++){
+	for(var i =0; i < globalJsonVar.length; i++){
 		if(globalJsonVar[i].stream == null){
-			$(".streams").append("<div class='stream'><span>"+globalJsonVar[i].display_name+"</span></div>");	
+			$(".streams").append("<div class='stream'><span>"+streamers[i]+"</span></div>");	
 		}
 		
 	}
+}
+
+function getData(){
+	for(var i = 0; i < streamers.length; i++){
+		$.getJSON('https://wind-bow.glitch.me/twitch-api/streams/'+streamers[i]+'', function(data){
+			globalJsonVar = globalJsonVar.concat(data); 	
+		});
+	} 
 }
 
 
